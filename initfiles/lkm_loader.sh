a0fb053e158aa25860d24c14ff9a29632c9b5a8e
Thomas Makin <halorocker89@gmail.com>
2023-04-18 20:32:15 -0400

    lkm: do not load therm_fan_est
    No longer built--using builtin tskin instead

--- initfiles/lkm_loader.sh
+++ initfiles/lkm_loader.sh
@@ -130,7 +130,6 @@ do_insmod /vendor/lib/modules/core.ko
 do_insmod /vendor/lib/modules/input-cfboost.ko
 do_insmod /vendor/lib/modules/pwm-tegra-pmc-blink.ko
 do_insmod /vendor/lib/modules/pwm_fan.ko
-do_insmod /vendor/lib/modules/therm_fan_est.ko
 
 # Joycons (make sure to load after pwm_fan to avoid excessive fanspin!)
 do_insmod /vendor/lib/modules/crc8.ko
