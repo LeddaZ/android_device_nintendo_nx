bd203e22d3f55bfaf965b4b9a28f092408cf3323
Thomas Makin <halorocker89@gmail.com>
2023-05-03 20:02:14 -0400

    nx: add properties from shield stock
    - enable vrr
    - disable sf vrr
    - allow hwc to override lcd density
    - some nvfwd stuff
    - some cec stuff
    - no deepisp because not supported

--- properties.mk
+++ properties.mk
@@ -8,15 +8,31 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # CEC
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hdmi.wake_on_hotplug=1
+	ro.hdmi.wake_on_hotplug=1 \
+	ro.hdmi.cec.source.playback_device_action_on_routing_control=wake_up_and_send_active_source
 
 # Charger
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     persist.sys.NV_ECO.IF.CHARGING=false
 
+# GameStream
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.gamestream.display.optimize=1
+
 # HWC
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.tegra.stb.mode=0
+
+# NVWFD
+PRODUCT_PROPERTY_OVERRIDES += \
+	nvwfd.gamemode=1 \
+	nvwfd.max_macroblocks=8160
+
+# SF
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.sf.vrr.enabled=1 \
+	ro.vendor.surface_flinger.use_frame_rate_api=false \
+	ro.vendor.sf.override_lcd_density=1
 
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
