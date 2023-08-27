c7b089dcd6a36df27ac456f63f9f86d172cce097
Thomas Makin <halorocker89@gmail.com>
2023-08-26 22:19:42 -0400

    nx: use typec usb hal

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ TARGET_TEGRA_OMX      ?= nvmm
 TARGET_TEGRA_PHS      ?= nvphs
 TARGET_TEGRA_POWER    ?= aosp
 TARGET_TEGRA_UBOOT    ?= nx
+TARGET_TEGRA_USB      ?= typec
 TARGET_TEGRA_WIDEVINE ?= true
 TARGET_TEGRA_WIFI     ?= bcm
 
